ddd4d7d61b4380b5be3927d6d6c47d79d8818193
Dmitry Samoilov <sukhov@localhost.localdomain>
2020-09-26 09:31:12 +0300

    Implemented the CsvReader class

---
+++ classes/csv_reader.rb
@@ -0,0 +1,20 @@
+class CsvReader
+  require 'csv'
+
+  def initialize(csv_file_name)
+    # Создаем переменную с путем до CSV файла
+    csv_file_dir = "csv_db/#{csv_file_name}"
+
+    # CSV.open возвращает 2-мерный массив
+    csv = CSV.open(csv_file_dir)
+    csv.map { |arr| reveal_integers(arr) }
+  end
+
+  private
+
+  # Меняет на числа те строки, который выражают собой числа
+  def reveal_integers(arr)
+    arr.map { |val| val.match?(/\A\d+\z/) ? val.to_i : val }
+  end
+
+end
